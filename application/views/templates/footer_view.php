bfd6b85476caf8c0fcecaf94b4b2482697040fcc
romdo <roman.d@brilliant-solutions.eu>
2014-12-12 17:19:45 +0200

    added qm and fixed

--- application/views/templates/footer_view.php
+++ application/views/templates/footer_view.php
@@ -16,9 +16,6 @@
 
 <?php include('quick_message.php');?>
 
-
-
-
 <!-- JavaScript -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
--- application/views/templates/quick_message.php
+++ application/views/templates/quick_message.php
@@ -1,5 +1,29 @@
 <div class="qm-body">
     <div class="qm-header"><span>New Comment</span><span class="pull-right close-qm"><i class="fa fa-times"></i></span></div>
     <div class="quick-message">
+        <?php $attributes = array('class' => 'quick-message-form form-horizontal', 'id' => 'qm-form', 'autocomplete' => 'off'); ?>
+        <?php echo form_open('#', $attributes); ?>
+        <div class="form-group qm" style="border-bottom:1px solid #ddd;">
+            <label for="inputType" class="col-md-2 control-label">To</label>
+            <div class="col-md-10">
+                <input type="text" class="form-control qm" id="inputType">
+            </div>
+        </div>
+        <div class="form-group qm" style="border-bottom:1px solid #ddd;">
+            <label for="inputType" class="col-md-2 control-label">Subject</label>
+            <div class="col-md-10">
+                <input type="text" class="form-control qm" id="inputType">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-md-12">
+                <div class="form-group">
+                    <textarea name="project_desc" id="qm-text" class="form-control qm" rows="14" style="padding-left: 15px;"></textarea>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <?php form_close( );?>
 </div>
 </div>
