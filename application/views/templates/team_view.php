f5461823101a1c4b8c5923315e9d9e1621ce69d7
romdo <roman.d@brilliant-solutions.eu>
2015-02-09 16:30:50 +0200

    added panel to team

--- application/views/templates/team_view.php
+++ application/views/templates/team_view.php
@@ -6,20 +6,189 @@
         <div class="row">
             <div class="col-md-12">
                 <p class="lead">Team</p>
-                <!-- Keep all page content within the page-content inset div! -->
-                        <?php foreach ($users as $ak => $u): ?>
-                            <ul>
-                                <?php if (($u['status'] == 1)): ?>
-                                    <li><span
-                                            class="label label-xs label-success label-round"></span><?php print($u['first_name'] . ' ' . $u['last_name']); ?>
-                                    </li>
-                                <?php else: ?>
-                                    <li><span
-                                            class="label label-xs label-default label-round"></span><?php print($u['first_name'] . ' ' . $u['last_name']); ?>
-                                    </li>
-                                <?php endif; ?>
-                            </ul>
-                        <?php endforeach ?>
+
+              <div class="row">
+                  <div class="col-md-3 col-sm-6">
+                      <div class="well well-sm" style="background-color: transparent; border-color: #BDBDBD;">
+                          <div class="media">
+                              <a class="thumbnail pull-left" href="#">
+                                  <img class="media-object" src="//placehold.it/80">
+                              </a>
+
+                              <div class="media-body">
+                                  <h4 class="media-heading">John Doe</h4>
+
+                                  <p><span class="label label-info">10 photos</span> <span class="label label-primary">89 followers</span>
+                                  </p>
+
+                                  <p>
+                                      <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-comment"></span>
+                                          Message</a>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+
+                  <div class="col-md-3 col-sm-6">
+                      <div class="well well-sm" style="background-color: transparent; border-color: #BDBDBD;">
+                          <div class="media">
+                              <a class="thumbnail pull-left" href="#">
+                                  <img class="media-object" src="//placehold.it/80">
+                              </a>
+
+                              <div class="media-body">
+                                  <h4 class="media-heading">John Doe</h4>
+
+                                  <p><span class="label label-info">10 photos</span> <span class="label label-primary">89 followers</span>
+                                  </p>
+
+                                  <p>
+                                      <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-comment"></span>
+                                          Message</a>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+
+                  <div class="col-md-3 col-sm-6">
+                      <div class="well well-sm" style="background-color: transparent; border-color: #BDBDBD;">
+                          <div class="media">
+                              <a class="thumbnail pull-left" href="#">
+                                  <img class="media-object" src="//placehold.it/80">
+                              </a>
+
+                              <div class="media-body">
+                                  <h4 class="media-heading">John Doe</h4>
+
+                                  <p><span class="label label-info">10 photos</span> <span class="label label-primary">89 followers</span>
+                                  </p>
+
+                                  <p>
+                                      <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-comment"></span>
+                                          Message</a>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+
+                  <div class="col-md-3 col-sm-6">
+                      <div class="well well-sm" style="background-color: transparent; border-color: #BDBDBD;">
+                          <div class="media">
+                              <a class="thumbnail pull-left" href="#">
+                                  <img class="media-object" src="//placehold.it/80">
+                              </a>
+
+                              <div class="media-body">
+                                  <h4 class="media-heading">John Doe</h4>
+
+                                  <p><span class="label label-info">10 photos</span> <span class="label label-primary">89 followers</span>
+                                  </p>
+
+                                  <p>
+                                      <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-comment"></span>
+                                          Message</a>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="col-md-3 col-sm-6">
+                      <div class="well well-sm" style="background-color: transparent; border-color: #BDBDBD;">
+                          <div class="media">
+                              <a class="thumbnail pull-left" href="#">
+                                  <img class="media-object" src="//placehold.it/80">
+                              </a>
+
+                              <div class="media-body">
+                                  <h4 class="media-heading">John Doe</h4>
+
+                                  <p><span class="label label-info">10 photos</span> <span class="label label-primary">89 followers</span>
+                                  </p>
+
+                                  <p>
+                                      <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-comment"></span>
+                                          Message</a>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+
+                  <div class="col-md-3 col-sm-6">
+                      <div class="well well-sm" style="background-color: transparent; border-color: #BDBDBD;">
+                          <div class="media">
+                              <a class="thumbnail pull-left" href="#">
+                                  <img class="media-object" src="//placehold.it/80">
+                              </a>
+
+                              <div class="media-body">
+                                  <h4 class="media-heading">John Doe</h4>
+
+                                  <p><span class="label label-info">10 photos</span> <span class="label label-primary">89 followers</span>
+                                  </p>
+
+                                  <p>
+                                      <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-comment"></span>
+                                          Message</a>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+
+                  <div class="col-md-3 col-sm-6">
+                      <div class="well well-sm" style="background-color: transparent; border-color: #BDBDBD;">
+                          <div class="media">
+                              <a class="thumbnail pull-left" href="#">
+                                  <img class="media-object" src="//placehold.it/80">
+                              </a>
+
+                              <div class="media-body">
+                                  <h4 class="media-heading">John Doe</h4>
+
+                                  <p><span class="label label-info">10 photos</span> <span class="label label-primary">89 followers</span>
+                                  </p>
+
+                                  <p>
+                                      <a href="#" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-comment"></span>
+                                          Message</a>
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+
+
+
+
+                  <!-- Keep all page content within the page-content inset div! -->
+<div class="col-sm-12">
+
+    <?php foreach ($users as $ak => $u): ?>
+        <ul>
+            <?php if (($u['status'] == 1)): ?>
+                <li><span
+                        class="label label-xs label-success label-round"></span><?php print($u['first_name'] . ' ' . $u['last_name']); ?>
+                </li>
+            <?php else: ?>
+                <li><span
+                        class="label label-xs label-default label-round"></span><?php print($u['first_name'] . ' ' . $u['last_name']); ?>
+                </li>
+            <?php endif; ?>
+        </ul>
+    <?php endforeach ?>
+</div>
+           </div>
             </div>
         </div>
     </div>
