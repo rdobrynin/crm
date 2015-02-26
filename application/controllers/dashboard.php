565477d315a8784eb142b930e5bab356ad3cd816
romdo <roman.d@brilliant-solutions.eu>
2015-02-26 16:21:18 +0200

    added getUSerroles

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -136,10 +136,9 @@ class Dashboard extends CI_Controller {
         $data['user'] = $this->admin_model->get_user_id($_SESSION['username']);
 
 
-        $user_role = $this->admin_model->getUserRole('14','5');
-        $user_roles = $this->admin_model->getUserRoles('14');
-        var_dump($user_roles);
-        exit();
+//        $data['user_role'] = $this->admin_model->getUserRole('14','5');
+//        $user_roles = $this->admin_model->getUserRoles('14');
+
 
         $overtasks = $this->task_model->getOverdueTasks();
 
